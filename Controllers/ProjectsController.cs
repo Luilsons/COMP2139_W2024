f5ca1f7cb147f8ad256a1ef8d525c69e0c4403a6
luilson <luilsonms@hotmail.com>
2024-01-19 10:40:45 -0500

    Add project files.

---
+++ Controllers/ProjectsController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Lab1.Models;
+
+namespace Lab1.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        public IActionResult Index()
+        {
+            var projects = new List<Project>()
+            {
+             new Project {ProjectId = 1, Name = "Project 1", Description = "First Project" }
+            // Feel free to add more projects here
+        };
+
+            return View(projects);
+        }
+
+        private static List<Project> _projects = new List<Project>()
+        {
+            new Project {ProjectId = 1, Name = "Project 2", Description = "First Paginated Project" }
+            //Add sample projects here
+        };
+
+
+        [HttpGet]
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult Create(Project projects)
+        {
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            var project = new Project { ProjectId = id, Name = "Project " + id, Description = "Details of Project " + id };
+
+            return View(project);
+        }
+    }
+}
---
+++ Models/Project.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab1.Models
+{
+    public class Project
+    {
+        public int ProjectId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Status { get; set; }
+        
+
+    }
+}
